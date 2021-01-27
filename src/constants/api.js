9afeb8b93ed9b662541c25b32e0c96cd6d33347e
Eva Singh <eva.singh@hotmail.com>
2021-01-27 23:38:57 +0100

    Add api key

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-const KEY = "your-key";
+const KEY = "5ffdca2cbfee1036185d100b";
 export const BASE_URL = "https://us-central1-noroff-final-exam.cloudfunctions.net/api/v1/";
 export const headers = {
     "Content-Type": "application/json",
