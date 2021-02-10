2468d704a5fc513b6224c9aff2f24a65230913dd
Eva Singh <eva.singh@hotmail.com>
2021-02-10 14:34:46 +0100

    Create components to delete messages and enquiries

---
+++ src/components/admin/DeleteMessage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { confirmAlert } from "react-confirm-alert";
+import Button from "react-bootstrap/Button";
+import { BASE_URL, headers, DELETE } from "../../constants/api";
+
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+function DeleteMessage(props) {
+  const history = useHistory();
+
+  function checkDelete() {
+    confirmAlert({
+      title: "Confirm deletion",
+      buttons: [
+        {
+          label: "yes",
+          onClick: () => deleteMessage(),
+        },
+        {
+          label: "no",
+        },
+      ],
+    });
+  }
+
+  async function deleteMessage() {
+    const url = BASE_URL + "contacts/" + props.id;
+    const options = { headers, method: DELETE };
+    await fetch(url, options);
+    history.push("/admin/messages");
+  }
+
+  return (
+    <Button variant="danger" onClick={checkDelete}>
+      Delete
+    </Button>
+  );
+}
+
+export default DeleteMessage;
