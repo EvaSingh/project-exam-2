573f5ff6b94c01dd9c3928860d8040b4c0a8df50
Eva Singh <eva.singh@hotmail.com>
2021-02-10 14:46:58 +0100

    Update

--- src/components/admin/Enquieries.js
+++ src/components/admin/Enquieries.js
@@ -1,12 +1,51 @@
 import React, { useState, useEffect } from "react";
+import { BASE_URL, headers } from "../../constants/api";
+import NavLink from "react-bootstrap/NavLink";
 import Container from "react-bootstrap/Container";
 import Heading from "../layout/Heading";
 
-function Enquieries() {
-    
+
+function Enquiries() {
+  const [enquiries, setEnquiries] = useState([]);
+  const [error, setError] = useState(null);
+
+  const url = BASE_URL + "enquiries";
+
+  const options = { headers };
+
+  useEffect(() => {
+      fetch(url, options)
+          .then((response) => response.json())
+          .then((json) => {
+              console.log(json);
+              // handle error
+              if (json.error) {
+                setEnquiries([]);
+                  setError(json.message);
+              } else {
+                setEnquiries(json);
+              }
+          })
+          .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <Heading title="Enquieries"/>           
+  <>
+  <Heading title="Enquiries" subtitle="Your enquiries are listed here"/>
+  <Container>
+          {error && <div className="error">{error}</div>}
+          <ul>
+              {enquiries.map((enquiry) => {
+                  return (
+                      <li key={enquiry.id}>
+                          <NavLink to={`/admin/hotels/enquiry/view/${enquiry.id}`}>{enquiry.name}</NavLink>
+                      </li>
+                  );
+              })}
+          </ul>
+      </Container></>
+      
   );
 }
 
-export default Enquieries;
+export default Enquiries;
--- src/components/admin/Messages.js
+++ src/components/admin/Messages.js
@@ -1,13 +1,51 @@
 import React, { useState, useEffect } from "react";
+import { BASE_URL, headers } from "../../constants/api";
+import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Heading from "../layout/Heading";
 
 
 function Messages() {
-    
-    return (
-      <Heading title="Messages"/>          
-    );
+  const [messages, setMessages] = useState([]);
+  const [error, setError] = useState(null);
+
+  const url = BASE_URL + "contacts";
+
+  const options = { headers };
+
+  useEffect(() => {
+      fetch(url, options)
+          .then((response) => response.json())
+          .then((json) => {
+              console.log(json);
+              // handle error
+              if (json.error) {
+                  setMessages([]);
+                  setError(json.message);
+              } else {
+                  setMessages(json);
+              }
+          })
+          .catch((error) => console.log(error));
+  }, []);
+
+  return (
+  <>
+  <Heading title="Messages" subtitle="Your messages are listed here"/>
+  <Container>
+          {error && <div className="error">{error}</div>}
+          <ul>
+              {messages.map((message) => {
+                  return (
+                      <li key={message.id}>
+                          <NavLink to={`/admin/messages/view/${message.id}`}>{message.name}</NavLink>
+                      </li>
+                  );
+              })}
+          </ul>
+      </Container></>
+      
+  );
 }
 
 export default Messages;
